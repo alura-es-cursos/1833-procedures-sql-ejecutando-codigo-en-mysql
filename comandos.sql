7c01bc0d1eaa52442d24ae54d0b24d896b92cd46
Alvaro Camacho <60886111+ahcamachod@users.noreply.github.com>
2021-06-10 12:23:59 -0300

    Comandos del Aula 2

--- comandos.sql
+++ comandos.sql
@@ -2,4 +2,8 @@
 En este script encontrarás todos los comandos que ejecutaremos durante el desarrollo de nuestro entrenamiento
 
 ¡Muchos éxitos en tus estudios!
+
+Ahora que tu ambiente está preparado, llegó el momento de colocar en práctica tu código.
+
+¡Manos a la obra!
 */
