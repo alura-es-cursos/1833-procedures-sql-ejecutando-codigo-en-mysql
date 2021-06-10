c73364759a0091efdd237cc29f4fe476ba2e9982
Alvaro Camacho <60886111+ahcamachod@users.noreply.github.com>
2021-06-10 13:34:48 -0300

    Comandos del Aula 5

--- comandos.sql
+++ comandos.sql
@@ -206,3 +206,175 @@ END$$
 DELIMITER ;
 
 CALL mostrar_sabor('1101035');
+
+-- PROYECTO DEL AULA ANTERIOR 4
+
+SELECT * FROM tabla_de_clientes;
+
+SELECT FECHA_DE_NACIMIENTO FROM tabla_de_clientes
+WHERE DNI = '1471156710';
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `edad_clientes`(vdni VARCHAR(20))
+BEGIN
+DECLARE vresultado VARCHAR(50);
+DECLARE vfecha DATE;
+SELECT FECHA_DE_NACIMIENTO INTO vfecha FROM tabla_de_clientes WHERE DNI = vdni;
+IF 
+vfecha < '19950101'
+THEN
+SET vresultado = 'Cliente Viejo.';
+ELSE
+SET vresultado = 'Cliente Joven.';
+END IF;
+SELECT vresultado;
+END$$
+DELIMITER ;
+
+CALL edad_clientes('50534475787');
+
+CALL edad_clientes('5648641702');
+
+
+SELECT * FROM tabla_de_productos;
+
+/* 
+precio >= 12 producto costoso.
+precio >= 7 y precio < 12 producto asequible.
+precio < 7 producto barato.
+*/
+
+SELECT PRECIO_DE_LISTA FROM tabla_de_productos 
+WHERE CODIGO_DEL_PRODUCTO = '1000800';
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `precio_producto`(vcodigo VARCHAR(20))
+BEGIN
+DECLARE vresultado VARCHAR(40);
+DECLARE vprecio FLOAT;
+SELECT PRECIO_DE_LISTA INTO vprecio FROM tabla_de_productos 
+WHERE CODIGO_DEL_PRODUCTO = vcodigo;
+IF vprecio >=12 
+THEN SET vresultado = 'Producto costoso.';
+ELSEIF
+vprecio >= 7 AND vprecio < 12
+THEN SET vresultado = 'Producto asequible.';
+ELSE
+SET vresultado = 'Producto barato.';
+END IF;
+SELECT vresultado;
+END$$
+DELIMITER ;
+
+CALL precio_producto('1000801');
+
+CALL precio_producto('1013793');
+
+CALL precio_producto('1096818');
+
+
+SELECT DISTINCT SABOR FROM tabla_de_productos;
+
+/* 
+Maracuyá Rico
+Limón Rico
+Frutilla Rico
+Uva Rico
+Sandía Normal
+Mango Normal
+Los demás Comunes
+*/
+
+SELECT * FROM tabla_de_productos;
+
+SELECT SABOR FROM tabla_de_productos WHERE CODIGO_DEL_PRODUCTO = '1002767';
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `define_sabor`(vcodigo VARCHAR(20))
+BEGIN
+DECLARE vsabor VARCHAR(20);
+SELECT SABOR INTO vsabor FROM tabla_de_productos WHERE CODIGO_DEL_PRODUCTO = vcodigo;
+CASE vsabor
+WHEN 'Maracuyá' THEN SELECT 'Muy Rico';
+WHEN 'Limón' THEN SELECT 'Muy Rico';
+WHEN 'Frutilla' THEN SELECT 'Muy Rico';
+WHEN 'Uva' THEN SELECT 'Muy Rico';
+WHEN 'Sandía' THEN SELECT 'Normal';
+WHEN 'Mango' THEN SELECT 'Normal';
+ELSE SELECT 'Jugos comunes';
+END CASE;
+END$$
+DELIMITER ;
+
+CALL define_sabor('1096818');
+
+CALL define_sabor('243083');
+
+SELECT SABOR, CODIGO_DEL_PRODUCTO FROM tabla_de_productos;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `define_sabor_con_error`(vcodigo VARCHAR(20))
+BEGIN
+DECLARE vsabor VARCHAR(20);
+DECLARE mensajeerror VARCHAR(50);
+DECLARE CONTINUE HANDLER FOR 1339
+SET mensajeerror = 'Sabor no definido en ningún caso.';
+SELECT SABOR INTO vsabor FROM tabla_de_productos WHERE CODIGO_DEL_PRODUCTO = vcodigo;
+CASE vsabor
+WHEN 'Maracuyá' THEN SELECT 'Muy Rico';
+WHEN 'Limón' THEN SELECT 'Muy Rico';
+WHEN 'Frutilla' THEN SELECT 'Muy Rico';
+WHEN 'Uva' THEN SELECT 'Muy Rico';
+WHEN 'Sandía' THEN SELECT 'Normal';
+WHEN 'Mango' THEN SELECT 'Normal';
+END CASE;
+SELECT mensajeerror;
+END$$
+DELIMITER ;
+
+CALL define_sabor_con_error('1000800');
+
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `precio_producto_case`(vcodigo VARCHAR(20))
+BEGIN
+DECLARE vresultado VARCHAR(40);
+DECLARE vprecio FLOAT;
+SELECT PRECIO_DE_LISTA INTO vprecio FROM tabla_de_productos 
+WHERE CODIGO_DEL_PRODUCTO = vcodigo;
+CASE 
+WHEN vprecio >=12 THEN SET vresultado = 'Producto costoso.';
+WHEN vprecio >= 7 AND vprecio < 12 THEN SET vresultado = 'Producto asequible.';
+WHEN vprecio < 7 THEN SET vresultado = 'Producto barato.';
+END CASE;
+SELECT vresultado;
+END
+$$
+
+CALL precio_producto_case('1000801');
+
+CALL precio_producto_case('1013793');
+
+CALL precio_producto_case('1096818');
+
+
+CREATE TABLE tb_looping (ID INT);
+
+SELECT * FROM tb_looping;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `looping`(vinicial INT, vfinal INT)
+BEGIN
+DECLARE vcontador INT;
+DELETE FROM tb_looping;
+SET vcontador = vinicial;
+WHILE vcontador <= vfinal
+DO
+INSERT INTO tb_looping VALUES(vcontador);
+SET vcontador = vcontador+1;
+END WHILE;
+SELECT * FROM tb_looping;
+END$$
+DELIMITER ;
+
+CALL looping(1,10);
