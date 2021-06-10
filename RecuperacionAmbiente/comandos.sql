f8cefcee90d736b95fec62b955fa35b45b8c4b84
Alvaro Camacho <60886111+ahcamachod@users.noreply.github.com>
2021-06-10 13:46:19 -0300

    Preparación del Ambiente

---
+++ RecuperacionAmbiente/comandos.sql
@@ -0,0 +1,24 @@
+INSERT INTO tabla_de_productos (CODIGO_DEL_PRODUCTO,NOMBRE_DEL_PRODUCTO,SABOR,TAMANO,ENVASE,PRECIO_DE_LISTA)
+     VALUES ('1001001','Sabor Alpino','Mango','700 ml','Botella',7.50),
+         ('1001000','Sabor Alpino','Melón','700 ml','Botella',7.50),
+         ('1001002','Sabor Alpino','Guanábana','700 ml','Botella',7.50),
+         ('1001003','Sabor Alpino','Mandarina','700 ml','Botella',7.50),
+         ('1001004','Sabor Alpino','Banana','700 ml','Botella',7.50),
+         ('1001005','Sabor Alpino','Asaí','700 ml','Botella',7.50),
+         ('1001006','Sabor Alpino','Mango','1 Litro','Botella',7.50),
+         ('1001007','Sabor Alpino','Melón','1 Litro','Botella',7.50),
+         ('1001008','Sabor Alpino','Guanábana','1 Litro','Botella',7.50),
+         ('1001009','Sabor Alpino','Mandarina','1 Litro','Botella',7.50),
+         ('1001010','Sabor Alpino','Banana','1 Litro','Botella',7.50),
+         ('1001011','Sabor Alpino','Asaí','1 Litro','Botella',7.50);
+
+
+SELECT * FROM tabla_de_productos WHERE NOMBRE_DEL_PRODUCTO LIKE 'Sabor Alp%';
+
+UPDATE tabla_de_productos SET PRECIO_DE_LISTA= 5 WHERE NOMBRE_DEL_PRODUCTO LIKE 'Sabor Alp%';
+
+SELECT * FROM tabla_de_productos WHERE NOMBRE_DEL_PRODUCTO LIKE 'Sabor Alp%';
+
+DELETE FROM tabla_de_productos WHERE NOMBRE_DEL_PRODUCTO LIKE 'Sabor Alp%';
+
+SELECT * FROM tabla_de_productos WHERE NOMBRE_DEL_PRODUCTO LIKE 'Sabor Alp%';
---
+++ RecuperacionAmbiente/inclusion_productos.sql
@@ -0,0 +1,16 @@
+INSERT INTO tb_producto (CODIGO,DESCRIPCION,SABOR,TAMANO,ENVASE,PRECIO_LISTA)
+     VALUES ('1001001','Sabor Alpino','Mango','700 ml','Botella',7.50),
+         ('1001000','Sabor Alpino','Melón','700 ml','Botella',7.50),
+         ('1001002','Sabor Alpino','Guanábana','700 ml','Botella',7.50),
+         ('1001003','Sabor Alpino','Mandarina','700 ml','Botella',7.50),
+         ('1001004','Sabor Alpino','Banana','700 ml','Botella',7.50),
+         ('1001005','Sabor Alpino','Asaí','700 ml','Botella',7.50),
+         ('1001006','Sabor Alpino','Mango','1 Litro','Botella',7.50),
+         ('1001007','Sabor Alpino','Melón','1 Litro','Botella',7.50),
+         ('1001008','Sabor Alpino','Guanábana','1 Litro','Botella',7.50),
+         ('1001009','Sabor Alpino','Mandarina','1 Litro','Botella',7.50),
+         ('1001010','Sabor Alpino','Banana','1 Litro','Botella',7.50),
+         ('1001011','Sabor Alpino','Asaí','1 Litro','Botella',7.50);
+
+
+SELECT * FROM tb_producto WHERE DESCRIPCION = 'Sabor Alpino';
