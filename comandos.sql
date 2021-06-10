af11d022d1bf080bc96ba23475b304d71d1ff78c
Alvaro Camacho <60886111+ahcamachod@users.noreply.github.com>
2021-06-10 12:42:47 -0300

    Comandos del Aula 3

--- comandos.sql
+++ comandos.sql
@@ -7,3 +7,105 @@ Ahora que tu ambiente está preparado, llegó el momento de colocar en práctica
 
 ¡Manos a la obra!
 */
+
+-- PROYECTO DEL AULA ANTERIOR 2
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `hola_mundo`()
+BEGIN
+SELECT "Hola Mundo!!!!";
+END$$
+DELIMITER ;
+
+CALL hola_mundo;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `muestra_numero`()
+BEGIN
+SELECT(9+5)*2 AS RESULTADO;
+END$$
+DELIMITER ;
+
+CALL muestra_numero;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `concatenar`()
+BEGIN
+SELECT CONCAT('Hola Mundo', " ", '!!!!') AS RESULTADO;
+END$$
+DELIMITER ;
+
+CALL concatenar;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `concatenar_con_comentarios`()
+BEGIN
+/*
+Este es un ejemplo de comentario
+al interior de un procedire
+*/
+-- Así se comenta un stored procedure.
+# Así también.
+SELECT CONCAT("Hola a todos!!", ' ', 'Este procedure concatena strings.') AS RESULTADO;
+END$$
+DELIMITER ;
+
+CALL concatenar_con_comentarios;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `concatenar2`()
+BEGIN
+SELECT CONCAT('Hola Mundo', " ", '!!!!') AS RESULTADO;
+END$$
+DELIMITER ;
+
+CALL concatenar2;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `exhibir_variable`()
+BEGIN
+DECLARE texto CHAR(20) DEFAULT 'Hola Mundo!!!!!!!';
+SELECT texto;
+END$$
+DELIMITER ;
+
+CALL exhibir_variable;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `declaracion`()
+BEGIN
+DECLARE v VARCHAR(30) DEFAULT "Caracteres Variables";
+DECLARE i INTEGER DEFAULT 564;
+DECLARE d DECIMAL(5,3) DEFAULT 89.765;
+DECLARE f DATE DEFAULT "2021-01-01";
+DECLARE ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP();
+SELECT v;
+SELECT i;
+SELECT d;
+SELECT f;
+SELECT ts;
+END$$
+DELIMITER ;
+
+CALL declaracion;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sin_declaracion`()
+BEGIN
+SELECT texto;
+END$$
+DELIMITER ;
+
+CALL sin_declaracion;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `atribuir_valores`()
+BEGIN
+DECLARE numero INTEGER DEFAULT 987;
+SELECT numero;
+SET numero = 324;
+SELECT numero;
+END$$
+DELIMITER ;
+
+CALL atribuir_valores;
