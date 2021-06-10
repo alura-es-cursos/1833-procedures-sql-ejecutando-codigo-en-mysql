ad5376d9fc7396375e66d25512f17aa2be3e5772
Alvaro Camacho <60886111+ahcamachod@users.noreply.github.com>
2021-06-10 12:45:37 -0300

    Comandos del Aula 4

--- comandos.sql
+++ comandos.sql
@@ -109,3 +109,100 @@ END$$
 DELIMITER ;
 
 CALL atribuir_valores;
+
+-- PROYECTO DEL AULA ANTERIOR 3
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `manipulacion`()
+BEGIN
+INSERT INTO tabla_de_productos (CODIGO_DEL_PRODUCTO,NOMBRE_DEL_PRODUCTO,SABOR,TAMANO,ENVASE,PRECIO_DE_LISTA)
+     VALUES ('1001001','Sabor Alpino','Mango','700 ml','Botella',7.50),
+         ('1001000','Sabor Alpino','Melón','700 ml','Botella',7.50),
+         ('1001002','Sabor Alpino','Guanábana','700 ml','Botella',7.50),
+         ('1001003','Sabor Alpino','Mandarina','700 ml','Botella',7.50),
+         ('1001004','Sabor Alpino','Banana','700 ml','Botella',7.50),
+         ('1001005','Sabor Alpino','Asaí','700 ml','Botella',7.50),
+         ('1001006','Sabor Alpino','Mango','1 Litro','Botella',7.50),
+         ('1001007','Sabor Alpino','Melón','1 Litro','Botella',7.50),
+         ('1001008','Sabor Alpino','Guanábana','1 Litro','Botella',7.50),
+         ('1001009','Sabor Alpino','Mandarina','1 Litro','Botella',7.50),
+         ('1001010','Sabor Alpino','Banana','1 Litro','Botella',7.50),
+         ('1001011','Sabor Alpino','Asaí','1 Litro','Botella',7.50);
+
+
+SELECT * FROM tabla_de_productos WHERE NOMBRE_DEL_PRODUCTO LIKE 'Sabor Alp%';
+
+UPDATE tabla_de_productos SET PRECIO_DE_LISTA= 5 WHERE NOMBRE_DEL_PRODUCTO LIKE 'Sabor Alp%';
+
+SELECT * FROM tabla_de_productos WHERE NOMBRE_DEL_PRODUCTO LIKE 'Sabor Alp%';
+
+DELETE FROM tabla_de_productos WHERE NOMBRE_DEL_PRODUCTO LIKE 'Sabor Alp%';
+
+SELECT * FROM tabla_de_productos WHERE NOMBRE_DEL_PRODUCTO LIKE 'Sabor Alp%';
+END$$
+DELIMITER ;
+
+CALL manipulacion;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `incluir_producto`()
+BEGIN
+DECLARE vcodigo VARCHAR(20) DEFAULT '3003001';
+DECLARE vnombre VARCHAR(20) DEFAULT 'Sabor Intenso';
+DECLARE vsabor VARCHAR(20) DEFAULT 'Tutti Frutti';
+DECLARE vtamano VARCHAR(20) DEFAULT '700 ml';
+DECLARE venvase VARCHAR(20) DEFAULT 'Botella PET';
+DECLARE vprecio DECIMAL(4,2) DEFAULT 7.25;
+INSERT INTO tabla_de_productos (CODIGO_DEL_PRODUCTO,NOMBRE_DEL_PRODUCTO,SABOR,TAMANO,ENVASE,PRECIO_DE_LISTA)
+     VALUES (vcodigo, vnombre, vsabor, vtamano, venvase, vprecio);
+END$$
+DELIMITER ;
+
+CALL incluir_producto;
+
+SELECT * FROM tabla_de_productos WHERE NOMBRE_DEL_PRODUCTO LIKE 'Sabor In%';
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `incluir_producto_parametros`(vcodigo VARCHAR(20), vnombre VARCHAR(20),vsabor VARCHAR(20),
+vtamano VARCHAR(20), venvase VARCHAR(20), vprecio DECIMAL(4,2))
+BEGIN
+DECLARE mensaje VARCHAR(40);
+DECLARE EXIT HANDLER FOR 1062
+BEGIN
+  SET mensaje = 'Producto duplicado! ';
+  SELECT mensaje;
+END;
+INSERT INTO tabla_de_productos (CODIGO_DEL_PRODUCTO,NOMBRE_DEL_PRODUCTO,SABOR,TAMANO,ENVASE,PRECIO_DE_LISTA)
+     VALUES (vcodigo, vnombre, vsabor, vtamano, venvase, vprecio);
+SET mensaje = 'Producto incluido con éxito!';
+SELECT mensaje;
+END$$
+DELIMITER ;
+
+CALL incluir_producto_parametros('1000800','Sabor del Mar',
+'Naranja', '700 ml', 'Botella de Vidrio', 2.25);
+
+SELECT * FROM tabla_de_productos WHERE CODIGO_DEL_PRODUCTO = '1000800';
+
+CALL incluir_producto_parametros('1000801','Sabor del Mar',
+'Naranja', '700 ml', 'Botella de Vidrio', 3.25);
+
+CALL incluir_producto_parametros('1000800','Sabor del Mar',
+'Naranja', '700 ml', 'Botella de Vidrio', 2.25);
+
+SELECT * FROM tabla_de_productos WHERE CODIGO_DEL_PRODUCTO = '1000800';
+
+SELECT CODIGO_DEL_PRODUCTO, SABOR FROM tabla_de_productos;
+
+SELECT SABOR FROM tabla_de_productos WHERE CODIGO_DEL_PRODUCTO = "1078680";
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `mostrar_sabor`(vcodigo VARCHAR(15))
+BEGIN
+DECLARE vsabor VARCHAR(20);
+SELECT SABOR INTO vsabor FROM tabla_de_productos WHERE CODIGO_DEL_PRODUCTO = vcodigo;
+SELECT vsabor;
+END$$
+DELIMITER ;
+
+CALL mostrar_sabor('1101035');
